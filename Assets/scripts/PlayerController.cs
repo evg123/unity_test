5eb492eda9460d0f682a724dc37fc61a98ad97a6
Eric VandeGriek <evandegriek@gmail.com>
2017-05-01 21:18:04 -0400

    new project

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rbody;
+
+    // Use this for initialization
+    void Start () {
+        rbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horiz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+
+        Vector3 move_vec = new Vector3(horiz, 0, vert);
+        move_vec *= speed;
+
+        transform.Translate(move_vec);
+    }
+}
